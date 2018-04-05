7405c3444d3b1e627f1a803631987b284d725c4b
Alexandru Radulescu <alexandru.razvan.radulescu@gmail.com>
2018-04-05 19:27:41 +0300

    login update

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ClarityModule } from '@clr/angular';
+import { CommonModule } from '@angular/common';
+import { PagesRoutingModule } from './pages/pages-routing.module';
+import { AppComponent } from './app.component';
+import { PagesModule } from './pages/pages.module';
+import { AppRoutingModule } from './app-routing.module';
+import { UserService } from './user.service';
+import { AuthGuard } from './auth.guard';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    AppRoutingModule,
+    CommonModule,
+    PagesModule,
+    BrowserModule,
+    ClarityModule,
+    PagesRoutingModule
+  ],
+  providers: [UserService, AuthGuard],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
